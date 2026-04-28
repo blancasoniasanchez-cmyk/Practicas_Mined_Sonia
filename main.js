784a7be744c399d916a01133f54858384fdf0b0c
blancasoniasanchez-cmyk <blanca.sonia.sanchez@clases.edu.sv>
2026-04-27 20:16:08 -0600

    agregando nuevo archivo

---
+++ main.js
@@ -0,0 +1,19 @@
+// sintaxis de javascript
+/* otro comentario */
+ 
+// declarando variables y constantes
+// variable
+var nombre = "Juan Pérez"; // cadena
+var nombre = "María"; //global
+ 
+let edad = 30; //int
+edad = 15;
+ 
+let decimal = 20.2; //double/float
+let esCasado = falso; //booleano
+ 
+// constante
+const PI = 3.1416; // double/float
+const SALUDO = "¡Hola Mundo!"; //cadena
+ 
+PI;
