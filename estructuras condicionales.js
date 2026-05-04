6d22d31050d57ca3f3f6a5fcb34641c17f36e1ef
blancasoniasanchez-cmyk <blanca.sonia.sanchez@clases.edu.sv>
2026-05-04 08:57:04 -0600

    agregar index para actividad grupo 17 de la semana 11

---
+++ estructuras condicionales.js
@@ -0,0 +1,31 @@
+// estructura condicional
+//if  -else (lo utilzamos cuando evaluamos una condición) 
+let edad = 20
+if (edad >=18)
+    console.log("felicidades puedes vota");
+} else {
+    console.log("aun no puedes votar, intenta en 5 años");
+}  
+
+//if/else/
+
+let nota = 7;
+ 
+if (nota >= 9 && nota <= 10) {
+
+    console.log('Excelente');
+
+} else if (nota >= 7 && nota < 9) {
+
+    console.log('Bueno');
+
+} else if (nota >= 5 && nota < 7) {
+
+    console.log('Regular');
+
+} else {
+
+    console.log('Reprobado');
+
+}
+ 
---
+++ estructuras repetitivas.js
@@ -0,0 +1,32 @@
+//repetir bloques de codigo (bucles)
+
+//ciclo for
+//imprimiendo los numero del 1 al 10
+/**
+ * Primer elemento 
+ */
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+
+
+
+consolelog ("....................................uso del while..........................")
+//ciclo while - cuando no sabemos hasto donde va a llegar el bucle
+//imprimiento los numeros del 1 al 10
+let j = 11;
+while (j <= 10) { //una condición
+    console.log(j)
+    j++;
+    }
+
+ console ("......................................uso de do while...........................")
+ //ciclo do while -- la primera vez siempre pasa (verdadera)
+ let k = 11;
+ do{
+    console.log(k)
+    k++;
+while(k <=10)  
+}
+
